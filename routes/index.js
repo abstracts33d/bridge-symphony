f37d472ae030cb84fa857b6238275d5f8ceff20a
Manuel Wegria <thisimla@gmail.com>
2017-05-27 16:57:10 +0100

    fix image format png to jpg

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,23 @@ var datafr={
   lang:'1',
 
   carousel_h1_1:'Bridge Symphony le meilleur atout des bridgeurs',
-  carousel_p_1:'',
   carousel_caption_1:'En Savoir Plus',
+  feature1_h2:'Enseignant ou joueur, le bridge a portée de main ',
+  feature1_h2_sub: 'Pratique ludique et d\'une parfaite visibilité',
+  feature1_p:'Bridge Symphony vous propose des miliers de possibilités d\'enchères a partir de 104 cartes declinant 26 donnes simulées resepcant les probabilités distributionelles (cf. typologie) que vous pouvez developper a loisir selon vos objectifs pedagogiques ou votre niveau personel',
 
-  carousel_h1_2:'Un support didactique pour l\'enseignement du bridge.',
-  carousel_p_2:'',
+  carousel_h1_2:'Un support didactique pour l\'enseignement du bridge ',
   carousel_caption_2:'En Savoir Plus',
+  feature2_h2:'',
+  feature2_h2_sub: '',
+  feature2_p:'',
 
-  carousel_h1_3:'Un outil de travail pour bridgeur de tout niveau.',
-  carousel_p_3:'',
+  carousel_h1_3:'Un outil de travail pour bridgeur de tout niveau ',
   carousel_caption_3:'En Savoir Plus',
+  feature3_h2:'',
+  feature3_h2_sub: '',
+  feature3_p:''
+
 }
 
 var dataen={
@@ -25,17 +32,22 @@ var dataen={
   lang:'2',
 
   carousel_h1_1:'Bridge Symphony the best trump for bridgers ',
-  carousel_p_1:'',
   carousel_caption_1:'Learn More',
+  feature1_h2:'Teacher or player the bridge in your hands ',
+  feature1_h2_sub: '',
+  feature1_p:'',
 
-  carousel_h1_2:'A didactic support for teaching bridge',
-  carousel_p_2:'',
+  carousel_h1_2:'A didactic support for teaching bridge ',
   carousel_caption_2:'Learn More',
+  feature2_h2:'',
+  feature2_h2_sub: '',
+  feature2_p:'',
 
-  carousel_h1_3:'A pedagogic tool for all level bridge player',
-  carousel_p_3:'',
+  carousel_h1_3:'A pedagogic tool for all level bridge player ',
   carousel_caption_3:'Learn More',
-
+  feature3_h2:'',
+  feature3_h2_sub: '',
+  feature3_p:'',
 }
 
 /* GET home page. */
