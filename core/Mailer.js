363d868ec293bfcad9d2597226e6e3aee1f1ee33
Manuel Wegria <thisimla@gmail.com>
2017-05-28 14:31:08 +0100

    nodemailer

---
+++ core/Mailer.js
@@ -0,0 +1,34 @@
+var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+var configMail = require('../bin/config').mail;//my json configurations mail
+
+function Mailer(){
+  this.nodemailer = nodemailer;
+  this.transporter = this.createTransporter();
+}
+
+Mailer.prototype = {
+  createTransporter: function() {
+    return this.nodemailer.createTransport(smtpTransport({
+        host: configMail.host, //mail.example.com (your server smtp)
+        port: configMail.port, //2525 (specific port)
+        secureConnection: configMail.secureConnection, //true or false
+        auth: {
+            user: configMail.auth.user, //user@mydomain.com
+            pass: process.env.mail_contact_pwd //password from specific user mail
+        }
+    }));
+  },
+
+  send: function(mailOptions) {
+    // send mail with defined transport object
+    this.transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+    });
+  }
+};
+
+module.exports = Mailer;
--- routes/contact.js
+++ routes/contact.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+var Mailer = require('../core/Mailer'); //require this module
+var mail = new Mailer();
+
 var datafr={
   title: 'Bridge Symphony',
   active:'contact',
@@ -19,6 +22,22 @@ router.get('/', function(req, res, next) {
   if (req.query.lang) {req.session.lang=req.query.lang}
   if (req.session.lang==1) {res.render('contact', datafr);}
   if (req.session.lang==2) {res.render('contact', dataen);}
+
+  if ( (req.query.name) && (req.query.email) && (req.query.message) ){
+    //console.log(mail);
+
+    mail.createTransporter();
+    mail.send(
+      {
+        from: req.query.email,
+        to: "contact@bridgesymphony.com",
+        subject: req.query.name,
+        text: req.query.message
+      }
+    );
+
+  }
+
 });
 
 module.exports = router;
